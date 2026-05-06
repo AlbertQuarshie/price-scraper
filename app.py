6d5e99ad68ca51952d72e22ffeb01cdd4d17d8e5
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-06 21:42:16 +0300

    Scraping the books and their prices from the website.

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ url ="https://books.toscrape.com/"
 response=requests.get(url)
 
 soup = BeautifulSoup(response.text,"html.parser")
+
+
+books = soup.find_all("article", class_="product_pod")
+
+data =[]
+
+for book in books[:10]:
+    title = book.h3.a["title"]
+    price = float(book.find("p", class_="price_color").text.replace("£", ""))
+
+    data.append({
+        "Title": title,
+        "Price_GBP": price
+    })
