f23f7c9a69662783a0ed3aa6431623a4051f8502
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-07 16:59:52 +0300

    Adding error handling methods and comments

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ import pandas as pd
 from datetime import datetime
 from tabulate import tabulate
 
-
+#User inputs currency code (In Caps)
 currency = input("Enter currency (e.g. KES, USD, EUR): ").upper()
 
+
+# Source Website
 url ="https://books.toscrape.com/"
-response=requests.get(url)
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print("Error accessing website:", e)
+    exit()
 
 soup = BeautifulSoup(response.text,"html.parser")
 
@@ -17,18 +25,36 @@ books = soup.find_all("article", class_="product_pod")
 
 data =[]
 
+# 10 books are scrapped
 for book in books[:10]:
-    title = book.h3.a["title"]
-    price = float(book.find("p", class_="price_color").text.replace("Â£", ""))
+    try:
+        title = book.h3.a["title"]
+        price = float(book.find("p", class_="price_color").text.replace("Â£", ""))
 
-    data.append({
-        "Title": title,
-        "Price_GBP": price
+        data.append({
+            "Title": title,
+            "Price_GBP": price
     })
+    
+    except Exception as e:
+        print("Error reading product")
 
-rate_api = "https://api.exchangerate-api.com/v4/latest/GBP"
-rate = requests.get(rate_api).json()["rates"][currency]
+# Initialization of exchange rate api to load in GBP as normal
+try:
+    rate_api = "https://api.exchangerate-api.com/v4/latest/GBP"
+    rate_response = requests.get(rate_api)
+    rate_response.raise_for_status()
 
+    rate = rate_response.json()["rates"][currency]
+
+except KeyError:
+    print("Invalid currency code.")
+    exit()
+
+except requests.exceptions.RequestException as e:
+    print("Error accessing currency API:", e)
+    exit()
+#Timestamp
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 for item in data:
@@ -37,11 +63,11 @@ for item in data:
 
 df = pd.DataFrame(data)
 
-
+#Tabulating data in the terminal
 print(tabulate(df, headers="keys", tablefmt="grid"))
 
-
+#Converting output to csv file
 df.to_csv("books.csv", index=False)
 
-
+#Confirmation message
 print("\nData saved to books.csv")
